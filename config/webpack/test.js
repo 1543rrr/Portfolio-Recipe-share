536a0a264614d77bb69ce9560289c586e63b22df
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-04-08 11:28:04 +0900

    トップページの作成

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,3 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+$(function(){
+  $("#test").css("color","red")
+  $(".hoge").css("color","#00ff7f")
+});
