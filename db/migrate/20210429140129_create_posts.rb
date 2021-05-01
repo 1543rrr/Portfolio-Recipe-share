5ca07bb1d497d46f4092143ee7b5f417af2cb162
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-05-01 11:59:03 +0900

    投稿機能の修正とトップページのロゴの変更

--- db/migrate/20210429140129_create_posts.rb
+++ db/migrate/20210429140129_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string :caption, null: false
-      t.string :photo,   null: false
+      t.string :photo
+      t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 2021_04_29_140129) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption", null: false
-    t.string "photo", null: false
+    t.string "photo"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_04_29_140129) do
   end
 
   add_foreign_key "photos", "posts"
+  add_foreign_key "posts", "users"
 end
